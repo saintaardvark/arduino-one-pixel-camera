ea93131a3d3661d63d646e2c6556dfc3bfafaddc
Hugh Brown (Saint Aardvark the Carpeted) <aardvark@saintaardvarkthecarpeted.com>
2023-11-25 11:00:42 -0800

    World's crappiest camera!

--- camera.ino
+++ camera.ino
@@ -22,16 +22,23 @@
   https://www.arduino.cc/en/Tutorial/BuiltInExamples/Blink
 */
 
+int lightSensor = 0;
+int val = 0;
+
+int SLEEPYTIME = 100;
+
 // the setup function runs once when you press reset or power the board
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(9600);
+  Serial.println("Hello, world!");
+  pinMode(lightSensor, INPUT);
 }
 
 // the loop function runs over and over again forever
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);                       // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-  delay(1000);                       // wait for a second
+  val = 1023 - analogRead(lightSensor);
+  Serial.println(val);
+  delay(SLEEPYTIME);
 }
