58a41b395e375595ecc4d0b4d65e7a7300d7eb6b
Hugh Brown (Saint Aardvark the Carpeted) <aardvark@saintaardvarkthecarpeted.com>
2023-11-29 13:02:35 -0800

    Add servo, just swing back & forth
    Code straight from
    https://docs.arduino.cc/learn/electronics/servo-motors.  All hail
    Arduino.cc for sharing!

--- camera.ino
+++ camera.ino
@@ -2,6 +2,12 @@
   Licensed under GPL v3.0.  See LICENSE.txt for details.
 */
 
+#include <Servo.h>
+
+Servo myservo;  // create servo object to control a servo
+int pos = 0;    // variable to store the servo position
+
+
 int lightSensor = 0;
 int val = 0;
 int i = 0;
@@ -14,23 +20,21 @@ void setup() {
   Serial.begin(9600);
   Serial.println("Hello, world!");
   pinMode(lightSensor, INPUT);
+  myservo.attach(9);  // attaches the servo on pin 9 to the servo object
 }
 
 // the loop function runs over and over again forever
 void loop() {
   i += 1;
-  val = 1023 - analogRead(lightSensor);
-  // 10 seconds divided by SLEEPYTIME
-  if (i % (10 * 1000 / SLEEPYTIME) == 0) {
-    for (int j=0; j < 10; j++) {
-      Serial.println("SLEEPYTIME");
-      delay(1000);
-    }
-  } else {
-    Serial.println(val);
-    if (i % (1000 / SLEEPYTIME ) == 0) {
-      Serial.println("*********************STEP");
-    }
-    delay(SLEEPYTIME);
+  for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
+    // in steps of 1 degree
+    myservo.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(15);                       // waits 15ms for the servo to reach the position
+		val = 1023 - analogRead(lightSensor);
+  }
+  for (pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
+    myservo.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(15);                       // waits 15ms for the servo to reach the position
   }
 }
+
