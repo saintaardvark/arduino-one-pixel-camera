28927b2d3152aca69e5da7d7660fa5d2fbcfbad8
Hugh Brown (Saint Aardvark the Carpeted) <aardvark@saintaardvarkthecarpeted.com>
2023-11-30 07:34:40 -0800

    WIP: Finer-grained adjustment of position

--- camera.ino
+++ camera.ino
@@ -4,14 +4,36 @@
 
 #include <Servo.h>
 
-Servo myservo;  // create servo object to control a servo
-int pos = 0;    // variable to store the servo position
-
+/* TODO: Rename these to x/y servo */
+Servo myservo1; // x
+Servo myservo2;	// y
+int xpos = 0;
+int ypos = 0;
 
 int lightSensor = 0;
 int val = 0;
 int i = 0;
 int SLEEPYTIME = 10;
+
+/* microseconds to sleep at each degree.  Since code currently assumes
+   10 sec to sweep, that's 10 s / 180 degrees = 56 microseconds
+*/
+int SERVO_DELAY = 56;
+
+/*
+  The initial setting of the position to 0 is quite fast; here we slow
+  that down.
+ */
+void resetServosSlowly() {
+  for (int i = myservo1.read(); i > 0 ; i--) {
+    myservo1.write(i);
+    delay(100);
+  }
+  for (int i = myservo2.read(); i > 0 ; i--) {
+    myservo1.write(i);
+    delay(100);
+  }
+}
 
 // the setup function runs once when you press reset or power the board
 void setup() {
@@ -20,21 +42,44 @@ void setup() {
   Serial.begin(9600);
   Serial.println("Hello, world!");
   pinMode(lightSensor, INPUT);
-  myservo.attach(9);  // attaches the servo on pin 9 to the servo object
+  myservo1.attach(9);  // attaches the servo on pin 9 to the servo object
+  myservo2.attach(8);  // attaches the servo on pin 9 to the servo object
+  resetServosSlowly();
+  delay(1000);
 }
+
+/*
+ Previously, this function used servo.write(degrees) -- and that's an
+ int, so we only get 180 values.
+
+ We can *also* use servo.writeMicroseconds(usec),which can go from 544
+ to 2400 (limits set in Servo.h).  This gets us 1856 values, a much
+ finer-grained movement.  Call that 1800 for simplicity.
+
+ TODO: Code written, compiles, not yet tested.
+ TODO: Logger will need to be adjusted as well
+ TODO: Break out 1800 to a constant (or just calculate it, which'd be better)
+ TODO: SERVO_DELAY will need to be adjusted? maybe?
+*/
 
 // the loop function runs over and over again forever
 void loop() {
-  i += 1;
-  for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
-    // in steps of 1 degree
-    myservo.write(pos);              // tell servo to go to position in variable 'pos'
-    delay(15);                       // waits 15ms for the servo to reach the position
-		val = 1023 - analogRead(lightSensor);
-  }
-  for (pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
-    myservo.write(pos);              // tell servo to go to position in variable 'pos'
-    delay(15);                       // waits 15ms for the servo to reach the position
+  for (ypos = 0; ypos <= 1800; ypos += 1) { // goes from 0 degrees to 180 degrees
+    myservo2.writeMicroseconds(MIN_PULSE_WIDTH + ypos); // tell servo to go to position in variable 'pos'
+    for (xpos = 0; xpos <= 1800; xpos += 1) { // goes from 0 degrees to 180 degrees
+      // in steps of 1 degree
+      myservo1.write(MIN_PULSE_WIDTH + xpos); // tell servo to go to position in variable 'pos'
+      delay(SERVO_DELAY); // waits  for the servo to reach the position
+      val = 1023 - analogRead(lightSensor);
+      Serial.println(val);
+    }
+    for (xpos = 1800; xpos >= 0; xpos -= 1) { // goes from 180 degrees to 0 degrees
+      myservo1.writeMicroseconds(MIN_PULSE_WIDTH + xpos);              // tell servo to go to position in variable 'pos'
+      delay(SERVO_DELAY);                       // waits 15ms for the servo to reach the position
+      val = 1023 - analogRead(lightSensor);
+      Serial.println(val);
+    }
+    Serial.println("YYYYY");
   }
 }
 
