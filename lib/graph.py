23d43366ab616e107fd7963466eb56507e7ab8be
Hugh Brown (Saint Aardvark the Carpeted) <aardvark@saintaardvarkthecarpeted.com>
2024-06-30 16:02:44 -0700

    Break out files in lib a bit better, add iqd.py

---
+++ lib/graph.py
@@ -0,0 +1,94 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+from matplotlib.colors import ListedColormap
+import numpy as np
+
+
+# TODO: Look at
+# https://stackoverflow.com/questions/28269157/plotting-in-a-non-blocking-way-with-matplotlib
+# for live plotting options.
+def graph(data: np.array, cmap: str = "gray", norm: str = "linear"):
+    """
+    Graph data in some way.  Args:
+
+    data: numpy array
+    cmap: color map for pcolormesh
+
+    norm: string of name of normalize for pcolormesh;
+    Normalize classes not supported.
+    """
+    print(f"Here's a graph!")
+    plt.imshow(data, cmap=cmap, norm=norm, origin="lower")
+    # plt.gca().set_aspect("equal")  # show square as square
+    plt.show()
+
+
+def compare(
+    df: pd.DataFrame,
+    title: str = "New",
+    cmap: str = "gray",
+    norm: str = "linear",
+    interp: str = "bicubic",
+):
+    """Compare plot of df vs the original"""
+    fig, axs = plt.subplots(2, 2)
+    fig.suptitle("Comparison")
+
+    axs[0][0].imshow(df, cmap=cmap, norm=norm, origin="lower")
+    axs[0][0].set_title("Original")
+    axs[0][0].label_outer()
+
+    axs[0][1].imshow(df, cmap=cmap, norm=norm, origin="lower", interpolation=interp)
+    axs[0][1].set_title(title)
+    axs[0][1].label_outer()
+
+    flat_df = df.to_numpy().flatten()
+    axs[1][0].hist(flat_df, bins=50, edgecolor="black")
+    axs[1][1].boxplot(flat_df)
+    plt.show()
+
+
+def clamp(df: pd.DataFrame, max: float = 65535.0) -> pd.DataFrame:
+    """
+    Clamp outlier values to some reasonable amount.
+    """
+
+
+def munge(data: list, x: int = 100, reverse: bool = True):
+    """
+    Munge data into 2d array, where x is specified
+    and y is int(len(data)/x).
+
+    If reverse is True, then assume every other line
+    needs to be reversed -- eg, because we're scanning
+    back and forth.
+    """
+    y = int(len(data) / x)
+
+    n = np.empty((y, x))
+    for i in range(int(len(data) / x)):
+        start = i * x
+        end = (i + 1) * x
+        line = data[start:end]
+        if i % 2 == 1 and reverse:
+            line = list(reversed(line))
+
+        n[i] = line
+
+    return n
+
+def avg_by_col(df: pd.DataFrame) -> pd.DataFrame:
+    """Calculate averages of pairs of columns
+    Assuming you want to average consecutive pairs 
+    (column 0 with column 1, column 2 with column 3, etc.)
+    """
+    num_columns = df.shape[1]
+    averaged_df = pd.DataFrame()
+    
+    for i in range(0, num_columns, 2):
+        if i + 1 < num_columns:
+            averaged_column = (df.iloc[:, i] + df.iloc[:, i + 1]) / 2
+            averaged_df[f'Averaged_{i}_{i+1}'] = averaged_column
+    return averaged_df
+
---
+++ lib/iqd.py
@@ -0,0 +1,73 @@
+from statistics import median
+
+import pandas as pd
+
+# See is_outlier below
+IQD_FILTER_FOR_NORMAL_DIST = 2.22
+MIN_ARRAY_LENGTH = 30  # Same as MAX_LENGTH in main.py.  TODO: Break out constants.py
+
+
+def iqd(a):
+    """
+    Return the interquartile distance of array a.
+    """
+    a = sorted(a)
+    mid = len(a) // 2
+    q1_median = median(a[:mid])
+    q3_median = median(a[mid:])
+    return q3_median - q1_median
+
+
+def clamp_df(df: pd.DataFrame, f: float = IQD_FILTER_FOR_NORMAL_DIST) -> pd.DataFrame:
+    """
+    Clamp a dataframe to a normal distribution.  See is_outlier()
+    for details.
+    """
+    flat_df = df.to_numpy().flatten()
+    iqr_dist = iqd(flat_df)
+    med = median(flat_df)
+    def _clamp(x):
+        if abs(x - med) > f * iqr_dist:
+            return 
+    return abs(m - med) > f * iqr_dist
+
+    return df.map(lambda x: ())
+
+
+def is_outlier(
+    a, m, f: float = IQD_FILTER_FOR_NORMAL_DIST, min_length=MIN_ARRAY_LENGTH
+):
+    """
+    Determine if measurement m is an outlier compared to
+    array a; return True if so.
+
+    Uses method outlined here: https://stackoverflow.com/questions/23199796/detect-and-exclude-outliers-in-a-pandas-dataframe/69001342#69001342
+
+    Quote:
+
+        Eliminate all data that is more than f times the interquartile
+        range away from the median of the data. That's also the
+        transformation that sklearn's RobustScaler uses for
+        example. IQR and median are robust to outliers, so you
+        outsmart the problems of the z-score approach.
+
+        In a normal distribution, we have roughly iqr=1.35*s, so you
+        would translate z=3 of a z-score filter to f=2.22 of an
+        iqr-filter. This will drop the 999 in the above example.
+
+        The basic assumption is that at least the "middle half" of
+        your data is valid and resembles the distribution well,
+        whereas you also mess up if your distribution has wide tails
+        and a narrow q_25% to q_75% interval.
+
+
+    Here we're just flagging values rather than eliminating them.
+
+    Exits early (& returns False) if a is too short.
+    """
+    if len(a) < min_length:
+        return False
+
+    iqr_dist = iqd(a)
+    med = median(a)
+    return abs(m - med) > f * iqr_dist
