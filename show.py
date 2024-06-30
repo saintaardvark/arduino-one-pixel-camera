a25734a2ac743a97227acaad424ff591fb465074
Hugh Brown (Saint Aardvark the Carpeted) <aardvark@saintaardvarkthecarpeted.com>
2024-06-30 16:02:01 -0700

    show.py: use click, add some convenience commands

--- show.py
+++ show.py
@@ -4,24 +4,54 @@
 #
 # Licensed under GPL v3.0.  See LICENSE.txt for details.
 
+import glob
+import os
 import sys
 
+import click
 import pandas as pd
 
 import lib
+
+
+@click.group()
+def show():
+    """
+    show a file
+    """
+    pass
+
+
+def display(filename: str):
+    """
+    display
+    """
+    df = pd.read_csv(filename)
+    lib.compare(df.T)
+
+
+@click.command()
+@click.option("--directory", default="data")
+def latest(directory):
+    # Ensure the directory path ends with a slash
+    list_of_files = glob.glob(f"{directory}/*.csv")
+    latest_file = max(list_of_files, key=os.path.getctime)
+    display(latest_file)
+
+
+@click.command()
+@click.option("--filename")
+def file(filename):
+    display(filename)
+
+show.add_command(latest)
+show.add_command(file)
 
 def main():
     """
     Main entry point
     """
-    print("Here we go!")
-    filename = sys.argv[1]
-    try:
-        df = pd.read_csv(filename)
-        lib.graph(df)
-    except Exception as e:
-        print(f"Error: {e}")
-        print("\n\nUsage: show.py [filename]")
+    show()
 
 
 if __name__ == "__main__":
